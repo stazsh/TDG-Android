ce0ddca93e24dc988f9e0ba2b5e6fe85dde7c5e2
jay-io <jay.dnb@outlook.in>
2022-10-26 00:20:22 +0530

    Add Fragment class `FragHome`

--- app/src/main/java/com/thedhobighat/FragHome.kt
+++ app/src/main/java/com/thedhobighat/FragHome.kt
@@ -1,59 +1,83 @@
 package com.thedhobighat
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragHome.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FragHome : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class FragHome() : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.frag_home, container, false)
-    }
+        val view = inflater.inflate(R.layout.frag_home, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragHome.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragHome().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        requireActivity().findViewById<BottomNavigationView>(R.id.bottom_navigation_view).menu.findItem(R.id.nav_home).isChecked = true
+
+        val sharedPreferences: SharedPreferences? = activity?.getSharedPreferences("TDG_APP", Context.MODE_PRIVATE)
+        sharedPreferences?.edit()?.putInt("active_fragment", 0)?.apply()
+
+        view.findViewById<ConstraintLayout>(R.id.clFormalWash).setOnClickListener {
+            val bundle = Bundle()
+            bundle.putString("type", "FORMAL_WASH")
+            val fragChooseItems = FragChooseItems()
+            fragChooseItems.arguments = bundle
+            parentFragmentManager.beginTransaction().apply {
+                setCustomAnimations(
+                    R.anim.slide_in_from_right,
+                    R.anim.slide_out_to_left,
+                    R.anim.slide_in_from_left,
+                    R.anim.slide_out_to_right
+                )
+                replace(R.id.main_fragment, fragChooseItems)
+                addToBackStack(null)
+                commit()
             }
+        }
+
+        view.findViewById<ConstraintLayout>(R.id.clDryWash).setOnClickListener {
+            val bundle = Bundle()
+            bundle.putString("type", "DRY_WASH")
+            val fragChooseItems = FragChooseItems()
+            fragChooseItems.arguments = bundle
+            parentFragmentManager.beginTransaction().apply {
+                setCustomAnimations(
+                    R.anim.slide_in_from_right,
+                    R.anim.slide_out_to_left,
+                    R.anim.slide_in_from_left,
+                    R.anim.slide_out_to_right
+                )
+                replace(R.id.main_fragment, fragChooseItems)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        view.findViewById<ConstraintLayout>(R.id.clSteamIron).setOnClickListener {
+            val bundle = Bundle()
+            bundle.putString("type", "STEAM_IRON")
+            val fragChooseItems = FragChooseItems()
+            fragChooseItems.arguments = bundle
+            parentFragmentManager.beginTransaction().apply {
+                setCustomAnimations(
+                    R.anim.slide_in_from_right,
+                    R.anim.slide_out_to_left,
+                    R.anim.slide_in_from_left,
+                    R.anim.slide_out_to_right
+                )
+                replace(R.id.main_fragment, fragChooseItems)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        return view
     }
 }
